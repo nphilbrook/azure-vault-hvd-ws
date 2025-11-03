b6a0fd2ca5e74b52e5ed9eb959406e1d18fac548
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-11-03 12:17:51 -0500

    eastus2 vault cluster and initial peering resources

--- prereqs.tf
+++ prereqs.tf
@@ -80,13 +80,14 @@ module "vault_prereqs" {
   # --- Key Vault "Bootstrap" Secrets --- #
   create_key_vault          = true
   kv_vault_license          = var.vault_license
-  kv_vault_cert_base64      = base64encode(local.cert)
-  kv_vault_privkey_base64   = base64encode(var.temp_cert_key)
-  kv_vault_ca_bundle_base64 = base64encode(local.ca)
+  kv_vault_cert_base64      = module.tls_certs_new_global.tls_fullchain_base64
+  kv_vault_privkey_base64   = module.tls_certs_new_global.tls_privkey_base64
+  kv_vault_ca_bundle_base64 = module.tls_certs_new_global.tls_ca_bundle_base64
 }
 
 # Auto-unseal key
 # Requires a KVAP with GetRotationPolicy (and other stuff)
+# Should probably make this depend on the full module above (KVAP needs to be created)
 resource "azurerm_key_vault_key" "vault_unseal_key" {
   name         = "vault-unseal-key-001"
   key_vault_id = module.vault_prereqs.key_vault_id
@@ -163,6 +164,7 @@ module "vault_prereqs_east2" {
 
 # Auto-unseal key
 # Requires a KVAP with GetRotationPolicy (and other stuff)
+# Should probably make this depend on the full module above (KVAP needs to be created)
 resource "azurerm_key_vault_key" "vault_unseal_key_east2" {
   name         = "vault-unseal-key-001"
   key_vault_id = module.vault_prereqs_east2.key_vault_id
@@ -178,3 +180,97 @@ resource "azurerm_key_vault_key" "vault_unseal_key_east2" {
     "unwrapKey",
   ]
 }
+
+#------------------------------------------------------------------------------
+# VNet Peering Between Central US and East US 2
+#------------------------------------------------------------------------------
+
+# Peering: Central US -> East US 2
+resource "azurerm_virtual_network_peering" "centralus_to_eastus2" {
+  name                      = "peer-centralus-to-eastus2"
+  resource_group_name       = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.resource_group_name
+  virtual_network_name      = module.vault_prereqs.vnet_name
+  remote_virtual_network_id = module.vault_prereqs_east2.vnet_id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = false
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+# Peering: East US 2 -> Central US
+resource "azurerm_virtual_network_peering" "eastus2_to_centralus" {
+  name                      = "peer-eastus2-to-centralus"
+  resource_group_name       = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
+  virtual_network_name      = module.vault_prereqs_east2.vnet_name
+  remote_virtual_network_id = module.vault_prereqs.vnet_id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = false
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+#------------------------------------------------------------------------------
+# NSG Rules for Cross-Region Vault Communication
+#------------------------------------------------------------------------------
+
+# # Central US: Allow inbound 8200 and 8201 from East US 2 vault subnet
+# resource "azurerm_network_security_rule" "centralus_allow_eastus2_vault_replication" {
+#   name                        = "AllowEastUS2VaultReplication"
+#   priority                    = 150
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_ranges     = ["8200", "8201"]
+#   source_address_prefix       = "10.128.1.0/26" # East US 2 vault subnet
+#   destination_address_prefix  = "10.128.6.0/25" # Central US vault subnet
+#   resource_group_name         = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.resource_group_name
+#   network_security_group_name = module.vault_prereqs.vault_nsg_name
+# }
+
+# # East US 2: Allow inbound 8200 and 8201 from Central US vault subnet
+# resource "azurerm_network_security_rule" "eastus2_allow_centralus_vault_replication" {
+#   name                        = "AllowCentralUSVaultReplication"
+#   priority                    = 150
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_ranges     = ["8200", "8201"]
+#   source_address_prefix       = "10.128.6.0/25" # Central US vault subnet
+#   destination_address_prefix  = "10.128.1.0/26" # East US 2 vault subnet
+#   resource_group_name         = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
+#   network_security_group_name = module.vault_prereqs_east2.vault_nsg_name
+# }
+
+# # Central US: Deny all other traffic from East US 2 VNet
+# resource "azurerm_network_security_rule" "centralus_deny_other_eastus2" {
+#   name                        = "DenyOtherEastUS2Traffic"
+#   priority                    = 4090
+#   direction                   = "Inbound"
+#   access                      = "Deny"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "10.128.0.0/22" # East US 2 VNet
+#   destination_address_prefix  = "*"
+#   resource_group_name         = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.resource_group_name
+#   network_security_group_name = module.vault_prereqs.vault_nsg_name
+# }
+
+# # East US 2: Deny all other traffic from Central US VNet
+# resource "azurerm_network_security_rule" "eastus2_deny_other_centralus" {
+#   name                        = "DenyOtherCentralUSTraffic"
+#   priority                    = 4090
+#   direction                   = "Inbound"
+#   access                      = "Deny"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "10.128.4.0/22" # Central US VNet
+#   destination_address_prefix  = "*"
+#   resource_group_name         = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
+#   network_security_group_name = module.vault_prereqs_east2.vault_nsg_name
+# }
