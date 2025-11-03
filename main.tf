94c1e31ed4574ac0596111240697dcd9007146fe
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-11-03 13:00:57 -0500

    Delete aborted westcentralus DR prereqs

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "vault_hvd" {
   location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.location
   create_resource_group = false
   resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.resource_group_name
-  vault_fqdn            = "vault.${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.zone_name}"
+  vault_fqdn            = "vault.${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.zone_name}"
 
   #------------------------------------------------------------------------------
   # Networking
@@ -52,41 +52,41 @@ module "vault_hvd" {
   vault_version = "1.21.0+ent"
 }
 
-# module "vault_hvd_eastus2" {
+# module "vault_hvd_dr" {
 #   source  = "app.terraform.io/philbrook/vault-enterprise-hvd/azurerm"
 #   version = "0.1.5-alpha"
 #   #------------------------------------------------------------------------------
 #   # Common
 #   #------------------------------------------------------------------------------
-#   friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.environment}dr"
-#   location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.location
+#   friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.environment}dr"
+#   location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.location
 #   create_resource_group = false
-#   resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
-#   vault_fqdn            = "vault-dr.${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.zone_name}"
+#   resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
+#   vault_fqdn            = "vault-dr.${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.zone_name}"
 
 #   #------------------------------------------------------------------------------
 #   # Networking
 #   #------------------------------------------------------------------------------
-#   vnet_id                         = module.vault_prereqs_east2.vnet_id
-#   vault_subnet_id                 = module.vault_prereqs_east2.vault_subnet_id
-#   lb_subnet_id                    = module.vault_prereqs_east2.vault_subnet_id
+#   vnet_id                         = module.vault_prereqs_dr.vnet_id
+#   vault_subnet_id                 = module.vault_prereqs_dr.vault_subnet_id
+#   lb_subnet_id                    = module.vault_prereqs_dr.vault_subnet_id
 #   lb_is_internal                  = true
 #   create_vault_private_dns_record = true
-#   private_dns_zone_name           = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.zone_name
-#   private_dns_zone_rg             = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
+#   private_dns_zone_name           = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.zone_name
+#   private_dns_zone_rg             = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
 
 #   #------------------------------------------------------------------------------
 #   # Azure Key Vault installation secrets and unseal key
 #   #------------------------------------------------------------------------------
-#   prereqs_keyvault_rg_name               = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
-#   prereqs_keyvault_name                  = module.vault_prereqs_east2.key_vault_name
-#   vault_license_keyvault_secret_id       = module.vault_prereqs_east2.vault_license_kv_secret_id
-#   vault_tls_cert_keyvault_secret_id      = module.vault_prereqs_east2.vault_cert_kv_secret_id
-#   vault_tls_privkey_keyvault_secret_id   = module.vault_prereqs_east2.vault_privkey_kv_secret_id
-#   vault_tls_ca_bundle_keyvault_secret_id = module.vault_prereqs_east2.vault_ca_bundle_kv_secret_id
+#   prereqs_keyvault_rg_name               = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
+#   prereqs_keyvault_name                  = module.vault_prereqs_dr.key_vault_name
+#   vault_license_keyvault_secret_id       = module.vault_prereqs_dr.vault_license_kv_secret_id
+#   vault_tls_cert_keyvault_secret_id      = module.vault_prereqs_dr.vault_cert_kv_secret_id
+#   vault_tls_privkey_keyvault_secret_id   = module.vault_prereqs_dr.vault_privkey_kv_secret_id
+#   vault_tls_ca_bundle_keyvault_secret_id = module.vault_prereqs_dr.vault_ca_bundle_kv_secret_id
 
-#   vault_seal_azurekeyvault_vault_name      = module.vault_prereqs_east2.key_vault_name
-#   vault_seal_azurekeyvault_unseal_key_name = azurerm_key_vault_key.vault_unseal_key_east2.name
+#   vault_seal_azurekeyvault_vault_name      = module.vault_prereqs_dr.key_vault_name
+#   vault_seal_azurekeyvault_unseal_key_name = azurerm_key_vault_key.vault_unseal_key_dr.name
 
 #   #------------------------------------------------------------------------------
 #   # Compute
@@ -96,7 +96,7 @@ module "vault_hvd" {
 #   # Default is 6
 #   #   vmss_vm_count     = var.vault_vms_count
 #   # default is "Standard_D2s_v5"
-#   vm_sku            = "Standard_D2as_v6" # try Standard_D2ds_v6
+#   # vm_sku            = "Standard_D2as_v6" # try Standard_D2ds_v6
 
 #   # Oracle plugin
 #   # This won't work until we override the custom_data shell script to install the client libs - not in RHEL repos
@@ -104,3 +104,5 @@ module "vault_hvd" {
 #   #   additional_package_names = ["oracle-instantclient-basiclite", "oracle-instantclient-sqlplus", "oracle-instantclient-devel"]
 #   vault_version = "1.21.0+ent"
 # }
+
+# # TODO: global DNS with Azure Traffic Manager
--- prereqs.tf
+++ prereqs.tf
@@ -124,191 +124,191 @@ resource "azurerm_key_vault_key" "vault_unseal_key" {
   ]
 }
 
-module "vault_prereqs_east2" {
-  source  = "app.terraform.io/philbrook/prereqs/azurerm"
-  version = "1.0.0"
+# module "vault_prereqs_east2" {
+#   source  = "app.terraform.io/philbrook/prereqs/azurerm"
+#   version = "1.0.0"
 
-  # --- Common --- #
-  friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.environment}dr"
-  location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.location
-  resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
-  create_resource_group = false
-  common_tags           = local.default_tags
+#   # --- Common --- #
+#   friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.environment}dr"
+#   location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.location
+#   resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.resource_group_name
+#   create_resource_group = false
+#   common_tags           = local.default_tags
 
-  # --- DNS --- #
-  # Created in azure-core-infra-ws since I needed it for TLS verification
-  create_public_dns_zone = false
+#   # --- DNS --- #
+#   # Created in azure-core-infra-ws since I needed it for TLS verification
+#   create_public_dns_zone = false
 
-  create_private_dns_zone = true
-  # Kind of skeezy using the same name as the public zone, but we actually want them to be the same
-  private_dns_zone_name = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.zone_name
+#   create_private_dns_zone = true
+#   # Kind of skeezy using the same name as the public zone, but we actually want them to be the same
+#   private_dns_zone_name = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.eastus2.zone_name
 
-  # Created in the HVD module
-  create_private_dns_zone_vnet_link = false
+#   # Created in the HVD module
+#   create_private_dns_zone_vnet_link = false
 
-  # --- Networking --- #
-  create_vnet         = true
-  create_nat_gateway  = true
-  create_nsg_nat_rule = true
-  create_bastion      = true
-  vnet_cidr           = ["10.128.0.0/22"]
-  bastion_subnet_cidr = "10.128.0.0/24"
-  # Rolling with one subnet for everything
-  # lb_subnet_cidr                 = "10.128.6.0/24"
-  # Testing if a /26 will work for this
-  vault_subnet_cidr              = "10.128.1.0/26"
-  cidr_allow_ingress_bastion_ssh = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # --- Networking --- #
+#   create_vnet         = true
+#   create_nat_gateway  = true
+#   create_nsg_nat_rule = true
+#   create_bastion      = true
+#   vnet_cidr           = ["10.128.0.0/22"]
+#   bastion_subnet_cidr = "10.128.0.0/24"
+#   # Rolling with one subnet for everything
+#   # lb_subnet_cidr                 = "10.128.6.0/24"
+#   # Testing if a /26 will work for this
+#   vault_subnet_cidr              = "10.128.1.0/26"
+#   cidr_allow_ingress_bastion_ssh = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
 
-  # Will get to all of these through the bastion for now
-  # cidr_allow_ingress_lb_443      = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
-  # cidr_ingress_lb_allow_8200     = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
-  # cidr_ingress_vault_allow_8200  = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # Will get to all of these through the bastion for now
+#   # cidr_allow_ingress_lb_443      = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # cidr_ingress_lb_allow_8200     = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # cidr_ingress_vault_allow_8200  = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
 
-  # Taking a Zero-Trust posture here, since I'm using HCPt's global agent pool
-  # To lock this down, would need to run HCPt agents somewhere and allow-list those IPs
-  # in the 2nd variable
-  network_default_action       = "Allow"
-  key_vault_cidr_allow_ingress = [] # do I need this?
+#   # Taking a Zero-Trust posture here, since I'm using HCPt's global agent pool
+#   # To lock this down, would need to run HCPt agents somewhere and allow-list those IPs
+#   # in the 2nd variable
+#   network_default_action       = "Allow"
+#   key_vault_cidr_allow_ingress = [] # do I need this?
 
-  # Bastion
-  bastion_ssh_public_key = var.ssh_public_key
-  bastion_size           = "Standard_B2s"
+#   # Bastion
+#   bastion_ssh_public_key = var.ssh_public_key
+#   bastion_size           = "Standard_B2s"
 
-  # --- Key Vault "Bootstrap" Secrets --- #
-  create_key_vault          = true
-  kv_vault_license          = var.vault_license
-  kv_vault_cert_base64      = module.tls_certs_new_global.tls_fullchain_base64
-  kv_vault_privkey_base64   = module.tls_certs_new_global.tls_privkey_base64
-  kv_vault_ca_bundle_base64 = module.tls_certs_new_global.tls_ca_bundle_base64
-}
+#   # --- Key Vault "Bootstrap" Secrets --- #
+#   create_key_vault          = true
+#   kv_vault_license          = var.vault_license
+#   kv_vault_cert_base64      = module.tls_certs_new_global.tls_fullchain_base64
+#   kv_vault_privkey_base64   = module.tls_certs_new_global.tls_privkey_base64
+#   kv_vault_ca_bundle_base64 = module.tls_certs_new_global.tls_ca_bundle_base64
+# }
 
-# Auto-unseal key
-# Requires a KVAP with GetRotationPolicy (and other stuff)
-# Should probably make this depend on the full module above (KVAP needs to be created)
-resource "azurerm_key_vault_key" "vault_unseal_key_east2" {
-  name         = "vault-unseal-key-001"
-  key_vault_id = module.vault_prereqs_east2.key_vault_id
-  key_type     = "RSA"
-  key_size     = 2048
+# # Auto-unseal key
+# # Requires a KVAP with GetRotationPolicy (and other stuff)
+# # Should probably make this depend on the full module above (KVAP needs to be created)
+# resource "azurerm_key_vault_key" "vault_unseal_key_east2" {
+#   name         = "vault-unseal-key-001"
+#   key_vault_id = module.vault_prereqs_east2.key_vault_id
+#   key_type     = "RSA"
+#   key_size     = 2048
 
-  key_opts = [
-    "encrypt",
-    "decrypt",
-    "sign",
-    "verify",
-    "wrapKey",
-    "unwrapKey",
-  ]
-}
+#   key_opts = [
+#     "encrypt",
+#     "decrypt",
+#     "sign",
+#     "verify",
+#     "wrapKey",
+#     "unwrapKey",
+#   ]
+# }
 
-module "vault_prereqs_dr" {
-  source  = "app.terraform.io/philbrook/prereqs/azurerm"
-  version = "1.0.0"
+# module "vault_prereqs_dr" {
+#   source  = "app.terraform.io/philbrook/prereqs/azurerm"
+#   version = "1.0.0"
 
-  # --- Common --- #
-  friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.environment}dr"
-  location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.location
-  resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
-  create_resource_group = false
-  common_tags           = local.default_tags
+#   # --- Common --- #
+#   friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.environment}dr"
+#   location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.location
+#   resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
+#   create_resource_group = false
+#   common_tags           = local.default_tags
 
-  # --- DNS --- #
-  # Created in azure-core-infra-ws since I needed it for TLS verification
-  create_public_dns_zone = false
+#   # --- DNS --- #
+#   # Created in azure-core-infra-ws since I needed it for TLS verification
+#   create_public_dns_zone = false
 
-  create_private_dns_zone = true
-  # Kind of skeezy using the same name as the public zone, but we actually want them to be the same
-  private_dns_zone_name = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.zone_name
+#   create_private_dns_zone = true
+#   # Kind of skeezy using the same name as the public zone, but we actually want them to be the same
+#   private_dns_zone_name = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.zone_name
 
-  # Created in the HVD module
-  create_private_dns_zone_vnet_link = false
+#   # Created in the HVD module
+#   create_private_dns_zone_vnet_link = false
 
-  # --- Networking --- #
-  create_vnet         = true
-  create_nat_gateway  = true
-  create_nsg_nat_rule = true
-  create_bastion      = true
-  vnet_cidr           = ["10.128.8.0/22"]
-  bastion_subnet_cidr = "10.128.8.0/24"
-  # Rolling with one subnet for everything
-  # lb_subnet_cidr                 = "10.128.6.0/24"
-  # Testing if a /26 will work for this
-  vault_subnet_cidr              = "10.128.9.0/26"
-  cidr_allow_ingress_bastion_ssh = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # --- Networking --- #
+#   create_vnet         = true
+#   create_nat_gateway  = true
+#   create_nsg_nat_rule = true
+#   create_bastion      = true
+#   vnet_cidr           = ["10.128.8.0/22"]
+#   bastion_subnet_cidr = "10.128.8.0/24"
+#   # Rolling with one subnet for everything
+#   # lb_subnet_cidr                 = "10.128.6.0/24"
+#   # Testing if a /26 will work for this
+#   vault_subnet_cidr              = "10.128.9.0/26"
+#   cidr_allow_ingress_bastion_ssh = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
 
-  # Will get to all of these through the bastion for now
-  # cidr_allow_ingress_lb_443      = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
-  # cidr_ingress_lb_allow_8200     = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
-  # cidr_ingress_vault_allow_8200  = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # Will get to all of these through the bastion for now
+#   # cidr_allow_ingress_lb_443      = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # cidr_ingress_lb_allow_8200     = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
+#   # cidr_ingress_vault_allow_8200  = data.tfe_outputs.azure_hcp_control_outputs.nonsensitive_values.ingress_ips
 
-  # Taking a Zero-Trust posture here, since I'm using HCPt's global agent pool
-  # To lock this down, would need to run HCPt agents somewhere and allow-list those IPs
-  # in the 2nd variable
-  network_default_action       = "Allow"
-  key_vault_cidr_allow_ingress = [] # do I need this?
+#   # Taking a Zero-Trust posture here, since I'm using HCPt's global agent pool
+#   # To lock this down, would need to run HCPt agents somewhere and allow-list those IPs
+#   # in the 2nd variable
+#   network_default_action       = "Allow"
+#   key_vault_cidr_allow_ingress = [] # do I need this?
 
-  # Bastion
-  bastion_ssh_public_key = var.ssh_public_key
-  bastion_size           = "Standard_B2s"
+#   # Bastion
+#   bastion_ssh_public_key = var.ssh_public_key
+#   bastion_size           = "Standard_B2s"
 
-  # --- Key Vault "Bootstrap" Secrets --- #
-  create_key_vault          = true
-  kv_vault_license          = var.vault_license
-  kv_vault_cert_base64      = module.tls_certs_new_global.tls_fullchain_base64
-  kv_vault_privkey_base64   = module.tls_certs_new_global.tls_privkey_base64
-  kv_vault_ca_bundle_base64 = module.tls_certs_new_global.tls_ca_bundle_base64
-}
+#   # --- Key Vault "Bootstrap" Secrets --- #
+#   create_key_vault          = true
+#   kv_vault_license          = var.vault_license
+#   kv_vault_cert_base64      = module.tls_certs_new_global.tls_fullchain_base64
+#   kv_vault_privkey_base64   = module.tls_certs_new_global.tls_privkey_base64
+#   kv_vault_ca_bundle_base64 = module.tls_certs_new_global.tls_ca_bundle_base64
+# }
 
-# Auto-unseal key
-# Requires a KVAP with GetRotationPolicy (and other stuff)
-resource "azurerm_key_vault_key" "vault_unseal_key_westcentral" {
-  name         = "vault-unseal-key-001"
-  key_vault_id = module.vault_prereqs_dr.key_vault_id
-  key_type     = "RSA"
-  key_size     = 2048
+# # Auto-unseal key
+# # Requires a KVAP with GetRotationPolicy (and other stuff)
+# resource "azurerm_key_vault_key" "vault_unseal_key_westcentral" {
+#   name         = "vault-unseal-key-001"
+#   key_vault_id = module.vault_prereqs_dr.key_vault_id
+#   key_type     = "RSA"
+#   key_size     = 2048
 
-  key_opts = [
-    "encrypt",
-    "decrypt",
-    "sign",
-    "verify",
-    "wrapKey",
-    "unwrapKey",
-  ]
+#   key_opts = [
+#     "encrypt",
+#     "decrypt",
+#     "sign",
+#     "verify",
+#     "wrapKey",
+#     "unwrapKey",
+#   ]
 
-  # A permission resource in this module must be in place before Terraform can create the key
-  depends_on = [module.vault_prereqs_dr]
-}
+#   # A permission resource in this module must be in place before Terraform can create the key
+#   depends_on = [module.vault_prereqs_dr]
+# }
 
 #------------------------------------------------------------------------------
 # VNet Peering Between Central US and West Central US
 #------------------------------------------------------------------------------
 
 # Peering: Central US -> West Central US
-resource "azurerm_virtual_network_peering" "centralus_to_westcentralus" {
-  name                      = "peer-centralus-to-westcentralus"
-  resource_group_name       = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.resource_group_name
-  virtual_network_name      = module.vault_prereqs.vnet_name
-  remote_virtual_network_id = module.vault_prereqs_dr.vnet_id
+# resource "azurerm_virtual_network_peering" "centralus_to_westcentralus" {
+#   name                      = "peer-centralus-to-westcentralus"
+#   resource_group_name       = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.centralus.resource_group_name
+#   virtual_network_name      = module.vault_prereqs.vnet_name
+#   remote_virtual_network_id = module.vault_prereqs_dr.vnet_id
 
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = false
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
-}
+#   allow_virtual_network_access = true
+#   allow_forwarded_traffic      = false
+#   allow_gateway_transit        = false
+#   use_remote_gateways          = false
+# }
 
-# Peering: West Central US -> Central US
-resource "azurerm_virtual_network_peering" "westcentralus_to_centralus" {
-  name                      = "peer-westcentralus-to-centralus"
-  resource_group_name       = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
-  virtual_network_name      = module.vault_prereqs_dr.vnet_name
-  remote_virtual_network_id = module.vault_prereqs.vnet_id
+# # Peering: West Central US -> Central US
+# resource "azurerm_virtual_network_peering" "westcentralus_to_centralus" {
+#   name                      = "peer-westcentralus-to-centralus"
+#   resource_group_name       = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
+#   virtual_network_name      = module.vault_prereqs_dr.vnet_name
+#   remote_virtual_network_id = module.vault_prereqs.vnet_id
 
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = false
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
-}
+#   allow_virtual_network_access = true
+#   allow_forwarded_traffic      = false
+#   allow_gateway_transit        = false
+#   use_remote_gateways          = false
+# }
 
 #------------------------------------------------------------------------------
 # NSG Rules for Cross-Region Vault Communication
