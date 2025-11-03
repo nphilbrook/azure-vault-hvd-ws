3f15b55421345ba5a79c6884d268515837fc1f12
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-11-03 14:03:42 -0500

    Create central canadia vault

--- main.tf
+++ main.tf
@@ -52,57 +52,58 @@ module "vault_hvd" {
   vault_version = "1.21.0+ent"
 }
 
-# module "vault_hvd_dr" {
-#   source  = "app.terraform.io/philbrook/vault-enterprise-hvd/azurerm"
-#   version = "0.1.5-alpha"
-#   #------------------------------------------------------------------------------
-#   # Common
-#   #------------------------------------------------------------------------------
-#   friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.environment}dr"
-#   location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.location
-#   create_resource_group = false
-#   resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
-#   vault_fqdn            = "vault-dr.${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.zone_name}"
+module "vault_hvd_dr" {
+  source  = "app.terraform.io/philbrook/vault-enterprise-hvd/azurerm"
+  version = "0.1.5-alpha"
+  #------------------------------------------------------------------------------
+  # Common
+  #------------------------------------------------------------------------------
+  friendly_name_prefix  = "${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.environment}dr"
+  location              = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.location
+  create_resource_group = false
+  resource_group_name   = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
+  vault_fqdn            = "vault-dr.${data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.zone_name}"
 
-#   #------------------------------------------------------------------------------
-#   # Networking
-#   #------------------------------------------------------------------------------
-#   vnet_id                         = module.vault_prereqs_dr.vnet_id
-#   vault_subnet_id                 = module.vault_prereqs_dr.vault_subnet_id
-#   lb_subnet_id                    = module.vault_prereqs_dr.vault_subnet_id
-#   lb_is_internal                  = true
-#   create_vault_private_dns_record = true
-#   private_dns_zone_name           = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.zone_name
-#   private_dns_zone_rg             = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.resource_group_name
+  #------------------------------------------------------------------------------
+  # Networking
+  #------------------------------------------------------------------------------
+  vnet_id                         = module.vault_prereqs_dr.vnet_id
+  vault_subnet_id                 = module.vault_prereqs_dr.vault_subnet_id
+  lb_subnet_id                    = module.vault_prereqs_dr.vault_subnet_id
+  lb_is_internal                  = true
+  create_vault_private_dns_record = true
+  private_dns_zone_name           = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.zone_name
+  private_dns_zone_rg             = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.global.resource_group_name
 
-#   #------------------------------------------------------------------------------
-#   # Azure Key Vault installation secrets and unseal key
-#   #------------------------------------------------------------------------------
-#   prereqs_keyvault_rg_name               = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
-#   prereqs_keyvault_name                  = module.vault_prereqs_dr.key_vault_name
-#   vault_license_keyvault_secret_id       = module.vault_prereqs_dr.vault_license_kv_secret_id
-#   vault_tls_cert_keyvault_secret_id      = module.vault_prereqs_dr.vault_cert_kv_secret_id
-#   vault_tls_privkey_keyvault_secret_id   = module.vault_prereqs_dr.vault_privkey_kv_secret_id
-#   vault_tls_ca_bundle_keyvault_secret_id = module.vault_prereqs_dr.vault_ca_bundle_kv_secret_id
+  #------------------------------------------------------------------------------
+  # Azure Key Vault installation secrets and unseal key
+  #------------------------------------------------------------------------------
+  prereqs_keyvault_rg_name               = data.tfe_outputs.azure_core_infra_outputs.values.environment_info.dr.resource_group_name
+  prereqs_keyvault_name                  = module.vault_prereqs_dr.key_vault_name
+  vault_license_keyvault_secret_id       = module.vault_prereqs_dr.vault_license_kv_secret_id
+  vault_tls_cert_keyvault_secret_id      = module.vault_prereqs_dr.vault_cert_kv_secret_id
+  vault_tls_privkey_keyvault_secret_id   = module.vault_prereqs_dr.vault_privkey_kv_secret_id
+  vault_tls_ca_bundle_keyvault_secret_id = module.vault_prereqs_dr.vault_ca_bundle_kv_secret_id
 
-#   vault_seal_azurekeyvault_vault_name      = module.vault_prereqs_dr.key_vault_name
-#   vault_seal_azurekeyvault_unseal_key_name = azurerm_key_vault_key.vault_unseal_key_dr.name
+  vault_seal_azurekeyvault_vault_name      = module.vault_prereqs_dr.key_vault_name
+  vault_seal_azurekeyvault_unseal_key_name = azurerm_key_vault_key.vault_unseal_key_dr.name
 
-#   #------------------------------------------------------------------------------
-#   # Compute
-#   #------------------------------------------------------------------------------
-#   vm_ssh_public_key = var.ssh_public_key
-#   vm_os_image       = "redhat9"
-#   # Default is 6
-#   #   vmss_vm_count     = var.vault_vms_count
-#   # default is "Standard_D2s_v5"
-#   # vm_sku            = "Standard_D2as_v6" # try Standard_D2ds_v6
+  #------------------------------------------------------------------------------
+  # Compute
+  #------------------------------------------------------------------------------
+  vm_ssh_public_key = var.ssh_public_key
+  vm_os_image       = "redhat9"
+  # Default is 6
+  #   vmss_vm_count     = var.vault_vms_count
+  # default is "Standard_D2s_v5"
+  # vm_sku            = "Standard_D2as_v6" # try Standard_D2ds_v6
 
-#   # Oracle plugin
-#   # This won't work until we override the custom_data shell script to install the client libs - not in RHEL repos
-#   #   vault_plugin_urls        = ["https://releases.hashicorp.com/vault-plugin-database-oracle/0.13.0+ent/vault-plugin-database-oracle_0.13.0+ent_linux_amd64.zip"]
-#   #   additional_package_names = ["oracle-instantclient-basiclite", "oracle-instantclient-sqlplus", "oracle-instantclient-devel"]
-#   vault_version = "1.21.0+ent"
-# }
+  # Oracle plugin
+  # This won't work until we override the custom_data shell script to install the client libs - not in RHEL repos
+  #   vault_plugin_urls        = ["https://releases.hashicorp.com/vault-plugin-database-oracle/0.13.0+ent/vault-plugin-database-oracle_0.13.0+ent_linux_amd64.zip"]
+  #   additional_package_names = ["oracle-instantclient-basiclite", "oracle-instantclient-sqlplus", "oracle-instantclient-devel"]
+  vault_version = "1.21.0+ent"
+}
 
-# # TODO: global DNS with Azure Traffic Manager
+# TODO: global DNS with Azure Traffic Manager
+
